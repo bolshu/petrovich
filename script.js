982f3927e3cc45c597970243b67d099fc1c713f5
Vadim Akulin <vadimakulin@gmail.com>
2018-10-24 23:40:12 +0300

    Доделал скрипт для разных сценариев. Стили и разметка теперь как в макете

--- script.js
+++ script.js
@@ -10,8 +10,19 @@ var Auth = {
   method: 'POST'
 };
 
+var wrapper = document.querySelector('.wrapper');
+var errorBlock = document.querySelector('.error');
+var form = wrapper.querySelector('.form');
+
+var profile = wrapper.querySelector('.profile');
+var profileName = profile.querySelector('.profile__name');
+var profileStats = profile.querySelector('.profile__stats');
+var profileBonuses = profile.querySelector('.profile__bonuses');
+var profileDataButton = profile.querySelector('.profile__data-button');
+
 var getRequest = function(onSuccess, onError, url, method, data) {
   var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
   xhr.responseType = 'json';
   xhr.timeout = TIMEOUT;
 
@@ -35,21 +46,37 @@ var getRequest = function(onSuccess, onError, url, method, data) {
   xhr.send(data || null);
 };
 
-var form = document.querySelector('form');
+var onError = function(errorMessage) {
+  errorBlock.classList.add('error--visible');
+  errorBlock.textContent = errorMessage;
+};
 
 var onFormSubmit = function(evt) {
   evt.preventDefault();
   var formData = new FormData(form);
 
   var onSuccess = function(response) {
-    console.log(response);
-  };
+    errorBlock.classList.remove('error--visible');
+    errorBlock.textContent = '';
 
-  var onError = function(errorMessage) {
-    console.log(errorMessage);
+    form.classList.add('form--hidden');
+    profile.classList.add('profile--visible');
+    profileName.textContent = response.data.name;
   };
 
   getRequest(onSuccess, onError, Auth.url, Auth.method, formData);
 };
-
 form.addEventListener('submit', onFormSubmit);
+
+var onProfileDataButtonClick = function(evt) {
+  evt.preventDefault();
+
+  var onSuccess = function(response) {
+    profileStats.classList.add('profile__stats--visible');
+    profileBonuses.textContent = response.data.bonus;
+    profileDataButton.removeEventListener('click', onProfileDataButtonClick);
+  };
+
+  getRequest(onSuccess, onError, Load.url, Load.method);
+};
+profileDataButton.addEventListener('click', onProfileDataButtonClick);
